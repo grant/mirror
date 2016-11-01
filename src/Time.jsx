6eacc03a26270f0ed9e1dcc92bb5e1197aa7dfa8
Grant Timmerman <granttimmerman@gmail.com>
2016-10-31 21:01:23 -0700

    Added background color and working time

--- src/Time.jsx
+++ src/Time.jsx
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
+var dateFormat = require('dateformat');
 
+const DAYS_OF_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+/**
+ * Displays the current time
+ */
 class Time extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      time: new Date()
+    };
+
+    // Update the time every second
+    setInterval(this.updateTime.bind(this), 1000);
+  }
+
+  updateTime() {
+    this.setState({
+      time: new Date()
+    });
+  }
+
   render() {
+    const now = new Date();
+    const hourMinute = dateFormat(now, 'h:MM');
+    const dayOfWeek = DAYS_OF_WEEK[dateFormat(now, 'N')];
+    const date = dateFormat(now, 'mmmm d');
     return (
-      <ul className="Time">
-        <li className="hour-minute">11:33</li>
-        <li className="day-of-week">Friday</li>
-        <li className="date">January 29</li>
+      <ul className='Time'>
+        <li className='hour-minute'>{hourMinute}</li>
+        <li className='day-of-week'>{dayOfWeek}</li>
+        <li className='date'>{date}</li>
       </ul>
     );
   }
